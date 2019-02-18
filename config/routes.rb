fbd1da93379d9cad03c3ba63448c1569f2e0fc22
Remi Achji <achji.remi@gmail.com>
2019-02-18 15:20:27 +0100

    added columns to user table

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'pages#home'
-   resources :babies do
+  resources :babies do
     resources :reviews, only: [:new, :create]
     resources :bookings, only: [:new, :create]
   end
