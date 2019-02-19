17675675094859c73c4270ab93a52c87267591c0
Florence Martino <florence.martino.fm@gmail.com>
2019-02-19 12:34:53 +0100

    bookingview

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,9 +5,14 @@ class BookingsController < ApplicationController
   end
 
   def create
-    @booking = Booking.new(booking_params)
+    @booking = Booking.new(start_date: params[:booking][:start_date].to_date, end_date: params[:booking][:end_date].to_date)
     @booking.baby = Baby.find(params[:baby_id])
-    @booking.save
+    @booking.user = current_user
+    if @booking.save
+      redirect_to baby_path(@baby)
+    else
+      render :new
+    end
   end
 
   private
