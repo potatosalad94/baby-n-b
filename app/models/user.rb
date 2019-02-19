4f275da41ef7fb41bfc0a876d4dfa0e879bac3d8
Remi Achji <achji.remi@gmail.com>
2019-02-19 18:36:55 +0100

    adding photo for users + navbar modified

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :bookings
   validates :first_name, presence: true
   validates :last_name, presence: true
+  mount_uploader :photo, PhotoUploader
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
