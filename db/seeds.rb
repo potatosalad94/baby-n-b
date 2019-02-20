c500a75f01d1e195218b7e2661f7b149a15039b7
Susan HA <ha.susan@outlook.fr>
2019-02-20 11:18:58 +0100

    rajout de Review.destroy_all dans la seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 puts 'Cleaning database...'
+Review.destroy_all
+Booking.destroy_all
 Baby.destroy_all
 User.destroy_all
-Booking.destroy_all
 
 puts 'Creating babies...'
 
