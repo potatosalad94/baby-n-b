7fbde7256a92353ff198343f792bdd8b3ee7cc3f
samir aissioui <samir.aissioui@sciencespo.fr>
2019-02-22 11:45:55 +0100

    Add cencel btn for booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -49,6 +49,13 @@ class BookingsController < ApplicationController
     end
   end
 
+  def destroy
+    @booking = Booking.find(params[:id])
+    authorize @booking
+    @booking.destroy
+    redirect_to bookings_path
+  end
+
   private
 
   def total(booking)
